2bb474db7748371921892aedfc39fe6394c5be00
juanHerrera <portador1051@gmail.com>
2026-03-28 05:46:52 -0300

    Resolve problem in tempral file /users/users.jsx

--- src/app/users/page.js
+++
@@ -1,9 +0,0 @@
-import {getUsersData} from '@/app/lib/data.js'
-
-export default async function Page () {
-  const users = await getUsersData() 
-  
-  return users && <div> 
-    {users.map(user => <div key={user.id}>{user.nombre_user}</div>)}
-  </div>
-}
